1083d42b4698eaa97f2de09a498e11057a46d81b
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2019-12-13 22:40:24 -0800

    Added missing classes

---
+++ src/models/Location.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Google Maps plugin for Craft CMS
+ *
+ * Maps in minutes. Powered by Google Maps.
+ *
+ * @author    Double Secret Agency
+ * @link      https://plugins.doublesecretagency.com/
+ * @copyright Copyright (c) 2014 Double Secret Agency
+ */
+
+namespace doublesecretagency\googlemaps\models;
+
+use craft\base\Model;
+
+/**
+ * Class Location
+ * @since 4.0.0
+ */
+class Location extends Model
+{
+
+}
---
+++ src/services/ProximitySearch.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Google Maps plugin for Craft CMS
+ *
+ * Maps in minutes. Powered by Google Maps.
+ *
+ * @author    Double Secret Agency
+ * @link      https://plugins.doublesecretagency.com/
+ * @copyright Copyright (c) 2014 Double Secret Agency
+ */
+
+namespace doublesecretagency\googlemaps\services;
+
+use craft\base\Component;
+
+/**
+ * Class ProximitySearch
+ * @since 4.0.0
+ */
+class ProximitySearch extends Component
+{
+
+}
