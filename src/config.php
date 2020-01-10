7c24009e9eae0a4d41f996dccc47257bca717b85
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2020-01-09 21:25:44 -0800

    Example config file

---
+++ src/config.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Google Maps config.php
+ *
+ * This file exists only as a template for the Google Maps settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'google-maps.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+
+//    // Google Maps API keys (required)
+//    'serverKey' => getenv('GOOGLEMAPS_SERVERKEY'),
+//    'browserKey' => getenv('GOOGLEMAPS_BROWSERKEY'),
+
+//    // ipstack (only if using the ipstack service)
+//    'ipstackApiAccessKey' => getenv('IPSTACK_APIACCESSKEY'),
+
+//    // MaxMind (only if using the MaxMind service)
+//    'maxmindService' => getenv('MAXMIND_SERVICE'),
+//    'maxmindUserId' => getenv('MAXMIND_USERID'),
+//    'maxmindLicenseKey' => getenv('MAXMIND_LICENSEKEY'),
+
+];
