ea3d14b7baf291df83f10190a823972d1d59f662
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2020-01-09 21:25:44 -0800

    Example config file

---
+++ src/config.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is only an example.
+ *
+ * Copy this file to your `config` folder,
+ * and rename it to be `google-maps.php`.
+ *
+ * /config/google-maps.php
+ *
+ * Uncomment the Google Maps API keys. If you
+ * are using any of the geolocation services,
+ * you should uncomment those lines as well.
+ */
+
+return [
+
+//    // Google Maps API keys (required)
+//    'serverKey' => getenv('GOOGLEMAPS_SERVERKEY'),
+//    'browserKey' => getenv('GOOGLEMAPS_BROWSERKEY'),
+
+//    // ipstack (only if using the ipstack service)
+//    'ipstackApiAccessKey' => getenv('IPSTACK_APIACCESSKEY'),
+
+//    // MaxMind (only if using the MaxMind service)
+//    'maxmindService' => getenv('MAXMIND_SERVICE'),
+//    'maxmindUserId' => getenv('MAXMIND_USERID'),
+//    'maxmindLicenseKey' => getenv('MAXMIND_LICENSEKEY'),
+
+];
