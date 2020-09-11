54b162b8838771ba8dae5104ecaf5047cc5c9ebe
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2020-09-11 00:09:23 -0700

    DNA sequence in PHP

--- src/models/DynamicMap.php
+++ src/models/DynamicMap.php
@@ -15,8 +15,10 @@ use craft\base\Element;
 use craft\base\Model;
 use craft\helpers\Html;
 use craft\helpers\Json;
+use craft\helpers\StringHelper;
 use craft\helpers\Template;
 use Twig\Markup;
+use yii\base\Exception;
 
 /**
  * Class DynamicMap
@@ -28,7 +30,7 @@ class DynamicMap extends Model
     /**
      * @var array Collection of internal data representing a map to be rendered.
      */
-    private $_dna;
+    private $_dna = [];
 
     /*
      * TODO: Can these 3 properties be combined?
@@ -58,26 +60,6 @@ class DynamicMap extends Model
     // ========================================================================= //
 
     /**
-     * Initialize a Dynamic Map object.
-     *
-     * @param array|Element|Address $locations
-     * @param array $options
-     * @param array $config
-     */
-    public function __construct($locations = [], array $options = [], array $config = [])
-    {
-        // Initialize DNA
-        $this->_dna = [];
-
-        // Set the initial map and marker DNA
-        $this->_setMapDna($options);
-        $this->_setMarkersDna($locations);
-
-        // Call parent constructor
-        parent::__construct($config);
-    }
-
-    /**
      * Can't output directly as a string (unfortunately)
      * because `__toString` isn't compatible with
      * the `raw` filter (necessary to show an HTML tag).
@@ -89,20 +71,55 @@ class DynamicMap extends Model
         return 'To display a map, append `.html()` to the map object.';
     }
 
+    /**
+     * Initialize a Dynamic Map object.
+     *
+     * @param array|Element|Address $locations
+     * @param array $options
+     * @param array $config
+     */
+    public function __construct($locations = [], array $options = [], array $config = [])
+    {
+        // Initialize map
+        $this->_addMap($locations, $options);
+
+        // Call parent constructor
+        parent::__construct($config);
+    }
+
+    // ========================================================================= //
+
+    /**
+     * Add one or more markers to the map.
+     *
+     * @param array|Element|Address $locations
+     * @param array $options
+     */
+    public function markers($locations, array $options = [])
+    {
+        $this->_addMarkers($locations, $options);
+    }
+
     // ========================================================================= //
 
     public function html(): Markup
     {
+        // If no DNA, throw an error
+        if (!$this->_dna) {
+            throw new Exception('Model misconfigured. The map DNA is empty.');
+        }
 
+        // Alias map from DNA
+        $map =& $this->_dna[0];
 
-        // COMPILE DNA AT THE LAST MINUTE
-
-//        \Craft::dd($this->_dna);
-
+        // If the first item is not a map, throw an error
+        if ('map' != $map['type']) {
+            throw new Exception('Map model misconfigured. The chain must begin with a `.map()` segment.');
+        }
 
         // Compile map container
         $html = Html::modifyTagAttributes('<div>Loading map...</div>', [
-            'id' => $this->_dna['map']['id'],
+            'id' => $map['options']['id'],
             'class' => 'gm-map',
             'data-dna' => Json::encode($this->_dna),
         ]);
@@ -110,6 +127,8 @@ class DynamicMap extends Model
         // Return Markup
         return Template::raw($html);
     }
+
+    // ========================================================================= //
 
     /**
      * Return the immutable DNA array.
@@ -123,194 +142,215 @@ class DynamicMap extends Model
 
     // ========================================================================= //
 
-    /**
-     * Add one or more markers to the map.
-     *
-     * @param array|Element|Address $locations
-     * @param array $options
-     */
-    public function markers($locations, array $options = [])
+//    private function _setMapDna($options)
+//    {
+//        $mapDna = [];
+//
+////        // TEMP
+////        $options = [
+////            'id' => 'gm-map-1',
+////            'width' => null,
+////            'height' => null,
+////            'zoom' => null, // (uses fitBounds by default)
+////            'center' => null, // (uses fitBounds by default)
+////            'styles' => null,
+////            'mapOptions' => null,
+////            'markerOptions' => null,
+////            'infoWindowOptions' => null,
+////            'infoWindowTemplate' => null,
+////            'fields' => null,
+////        ];
+////        // ENDTEMP
+//
+//
+//        // Set the map ID
+//        if (isset($options['id'])) {
+//            $mapDna['id'] = $options['id'];
+//        } else {
+//            $mapCounter = 1; // TEMP
+//            $mapDna['id'] = "gm-map-{$mapCounter}";
+//        }
+//
+//        // If the width is specified
+//        if (isset($options['width'])) {
+//            $mapDna['width'] = $options['width'];
+//        }
+//
+//        // If the height is specified
+//        if (isset($options['height'])) {
+//            $mapDna['height'] = $options['height'];
+//        }
+//
+//        // If the zoom is specified
+//        if (isset($options['zoom'])) {
+//            $mapDna['zoom'] = $options['zoom'];
+//        }
+//
+//        // If the center is specified
+//        if (isset($options['center'])) {
+//            // Apply center
+//        }
+//
+//        // If the styles are specified
+//        if (isset($options['styles'])) {
+//            // Apply styles
+//        }
+//
+//        // If map options are specified
+//        if (isset($options['mapOptions'])) {
+//            // Apply map options
+//        }
+//
+//        // If marker options are specified, set as internal fallback
+//        if (isset($options['markerOptions'])) {
+//            $this->_defaultMarkerOptions = $options['markerOptions'];
+//        }
+//
+//        // If info window options are specified, set as internal fallback
+//        if (isset($options['infoWindowOptions'])) {
+//            $this->_defaultInfoWindowOptions = $options['infoWindowOptions'];
+//        }
+//
+//        // If the info window template is specified
+//        if (isset($options['infoWindowTemplate'])) {
+//            $this->_defaultInfoWindowTemplate = $options['infoWindowTemplate'];
+//        }
+//
+//        // If the fields are specified
+//        if (isset($options['fields'])) {
+//            // Apply fields
+//        }
+//
+////        \Craft::dd($mapDna);
+//
+//        $this->_dna['map'] = $mapDna;
+//
+//    }
+
+//    private function _setMarkersDna($locations, $options = [])
+//    {
+//        $markersDna = [];
+//
+//        // If location was specified as coordinates, nest them in an array
+//        if (isset($locations['lat']) && isset($locations['lng'])) {
+//            $locations = [$locations];
+//        }
+//
+//        // Force an array structure
+//        if (!is_array($locations)) {
+//            $locations = [$locations];
+//        }
+//
+//        // Loop through all locations
+//        foreach ($locations as $location) {
+//
+//            // Get coordinates (or collection of coordinates) from location
+//            $coordinates = $this->_setLocationCoords($location, $options);
+//
+//            // If invalid coordinates, skip location
+//            if (!$coordinates) {
+//                continue;
+//            }
+//
+//            // Add each marker to collection
+//            foreach ($coordinates as $coords) {
+//                $this->_dna['markers'][] = $this->_markerDna($coords, $options);
+//            }
+//
+//        }
+//
+//    }
+
+//    /**
+//     * Configure the DNA of a single marker.
+//     *
+//     * @param $coords
+//     * @param array $options
+//     * @return array
+//     */
+//    private function _markerDna($coords, array $options = []): array
+//    {
+////        // TEMP
+////        $options = [
+////            'icon' => null,
+////            'markerOptions' => null,
+////            'infoWindowOptions' => null,
+////            'infoWindowTemplate' => null,
+////            'fields' => null,
+////        ];
+////        // ENDTEMP
+//
+//        // Set values
+//        $markerOptions      = ($options['markerOptions']      ?? $this->_defaultMarkerOptions      ?? null);
+//        $infoWindowOptions  = ($options['infoWindowOptions']  ?? $this->_defaultInfoWindowOptions  ?? null);
+//        $infoWindowTemplate = ($options['infoWindowTemplate'] ?? $this->_defaultInfoWindowTemplate ?? null);
+//
+//
+//        // Return compiled marker DNA
+//        return [
+//            'coords' => $coords,
+//            'markerOptions' => $markerOptions,
+//        ];
+//    }
+
+    // ========================================================================= //
+
+    private function _addMap($locations, $options)
+    {
+        // Ensure options are a valid array
+        if (!$options || !is_array($options)) {
+            $options = [];
+        }
+
+        // If no ID, automatically generate a random one
+        if (!isset($options['id'])) {
+            $hash = StringHelper::randomString(6);
+            $options['id'] = "gm-map-{$hash}";
+        }
+
+        // Add map to DNA
+        $this->_dna[] = [
+            'type' => 'map',
+            'locations' => $this->_convertToCoords($locations),
+            'options' => $options,
+        ];
+    }
+
+    private function _addMarkers($locations, $options)
     {
         // If no locations were specified, bail
         if (!$locations) {
             return $this;
         }
 
-        $this->_setMarkersDna($locations, $options);
-
-        return $this;
-    }
-
-    // ========================================================================= //
-
-    private function _setMapDna($options)
-    {
-        $mapDna = [];
-
-//        // TEMP
-//        $options = [
-//            'id' => 'gm-map-1',
-//            'width' => null,
-//            'height' => null,
-//            'zoom' => null, // (uses fitBounds by default)
-//            'center' => null, // (uses fitBounds by default)
-//            'styles' => null,
-//            'mapOptions' => null,
-//            'markerOptions' => null,
-//            'infoWindowOptions' => null,
-//            'infoWindowTemplate' => null,
-//            'fields' => null,
-//        ];
-//        // ENDTEMP
-
-
-        // Set the map ID
-        if (isset($options['id'])) {
-            $mapDna['id'] = $options['id'];
-        } else {
-            $mapCounter = 1; // TEMP
-            $mapDna['id'] = "gm-map-{$mapCounter}";
-        }
-
-        // If the width is specified
-        if (isset($options['width'])) {
-            $mapDna['width'] = $options['width'];
-        }
-
-        // If the height is specified
-        if (isset($options['height'])) {
-            $mapDna['height'] = $options['height'];
-        }
-
-        // If the zoom is specified
-        if (isset($options['zoom'])) {
-            $mapDna['zoom'] = $options['zoom'];
-        }
-
-        // If the center is specified
-        if (isset($options['center'])) {
-            // Apply center
-        }
-
-        // If the styles are specified
-        if (isset($options['styles'])) {
-            // Apply styles
-        }
-
-        // If map options are specified
-        if (isset($options['mapOptions'])) {
-            // Apply map options
-        }
-
-        // If marker options are specified, set as internal fallback
-        if (isset($options['markerOptions'])) {
-            $this->_defaultMarkerOptions = $options['markerOptions'];
-        }
-
-        // If info window options are specified, set as internal fallback
-        if (isset($options['infoWindowOptions'])) {
-            $this->_defaultInfoWindowOptions = $options['infoWindowOptions'];
-        }
-
-        // If the info window template is specified
-        if (isset($options['infoWindowTemplate'])) {
-            $this->_defaultInfoWindowTemplate = $options['infoWindowTemplate'];
-        }
-
-        // If the fields are specified
-        if (isset($options['fields'])) {
-            // Apply fields
-        }
-
-//        \Craft::dd($mapDna);
-
-        $this->_dna['map'] = $mapDna;
-
-    }
-
-    private function _setMarkersDna($locations, $options = [])
-    {
-        $markersDna = [];
-
-        // If location was specified as coordinates, nest them in an array
-        if (isset($locations['lat']) && isset($locations['lng'])) {
-            $locations = [$locations];
-        }
-
-        // Force an array structure
-        if (!is_array($locations)) {
-            $locations = [$locations];
-        }
-
-        // Loop through all locations
-        foreach ($locations as $location) {
-
-            // Get coordinates (or collection of coordinates) from location
-            $coordinates = $this->_setLocationCoords($location, $options);
-
-            // If invalid coordinates, skip location
-            if (!$coordinates) {
-                continue;
-            }
-
-            // Add each marker to collection
-            foreach ($coordinates as $coords) {
-                $this->_dna['markers'][] = $this->_markerDna($coords, $options);
-            }
-
-        }
-
-    }
-
-    /**
-     * Configure the DNA of a single marker.
-     *
-     * @param $coords
-     * @param array $options
-     * @return array
-     */
-    private function _markerDna($coords, array $options = []): array
-    {
-//        // TEMP
-//        $options = [
-//            'icon' => null,
-//            'markerOptions' => null,
-//            'infoWindowOptions' => null,
-//            'infoWindowTemplate' => null,
-//            'fields' => null,
-//        ];
-//        // ENDTEMP
-
-        // Set values
-        $markerOptions      = ($options['markerOptions']      ?? $this->_defaultMarkerOptions      ?? null);
-        $infoWindowOptions  = ($options['infoWindowOptions']  ?? $this->_defaultInfoWindowOptions  ?? null);
-        $infoWindowTemplate = ($options['infoWindowTemplate'] ?? $this->_defaultInfoWindowTemplate ?? null);
-
-
-        // Return compiled marker DNA
-        return [
-            'coords' => $coords,
-            'markerOptions' => $markerOptions,
+        // Add markers to DNA
+        $this->_dna[] = [
+            'type' => 'markers',
+            'locations' => $this->_convertToCoords($locations),
+            'options' => $options,
         ];
     }
 
+
     // Always return coordinates within a parent array,
     // to compensate for Elements with multiple Addresses.
-    private function _setLocationCoords($location, $options)
+    private function _convertToCoords($locations)
     {
+
         // If location was specified as coordinates, return them as-is
-        if (isset($location['lat']) && isset($location['lng'])) {
-            return [$location];
+        if (isset($locations['lat']) && isset($locations['lng'])) {
+            return [$locations];
         }
+
+        return $locations;
 
 
         // CHECK TO SEE WHAT KIND OF THING $location IS
 
         // GET THE COORDS OF WHATEVER IT IS
 
-        \Craft::dd($location);
 
-
+        \Craft::dd($locations);
 
 
     }
