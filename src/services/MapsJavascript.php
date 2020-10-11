f4a766e1c05d68807bb8f09bc979faaa122e216f
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2020-10-11 15:32:48 -0700

    Deprecated object

--- src/services/MapsJavascript.php
+++ src/services/MapsJavascript.php
@@ -11,7 +11,7 @@
 
 namespace doublesecretagency\googlemaps\services;
 
-use doublesecretagency\googlemaps\models\_OLD_DynamicMap;
+//use doublesecretagency\googlemaps\models\_OLD_DynamicMap;
 
 /**
  * Class MapsJavascript
@@ -20,46 +20,46 @@ use doublesecretagency\googlemaps\models\_OLD_DynamicMap;
 class MapsJavascript extends Api
 {
 
-    /**
-     * @var int Counter for generating map IDs.
-     */
-    private $_mapCounter = 0;
-
-    /**
-     * @var array Internal collection of maps.
-     */
-    private $_maps = [];
-
-    /**
-     * Get a Dynamic Map model (new or existing).
-     *
-     * @param array|string $options
-     * @return _OLD_DynamicMap|false
-     */
-    public function getMap($options = [])
-    {
-        // If the options are actually a string
-        if (is_string($options)) {
-            // Return existing map
-            return ($this->_maps[$options] ?? false);
-        }
-
-        // Generate a new dynamic map
-        $map = new _OLD_DynamicMap($options);
-
-        // Alias for map ID
-        $id =& $map->dna['id'];
-
-        // If no map ID, set one
-        if (!$id) {
-            $id = 'gm-map-'.++$this->_mapCounter;
-        }
-
-        // Add new map to internal collection
-        $this->_maps[$id] = $map;
-
-        // Return new map
-        return $map;
-    }
+//    /**
+//     * @var int Counter for generating map IDs.
+//     */
+//    private $_mapCounter = 0;
+//
+//    /**
+//     * @var array Internal collection of maps.
+//     */
+//    private $_maps = [];
+//
+//    /**
+//     * Get a Dynamic Map model (new or existing).
+//     *
+//     * @param array|string $options
+//     * @return _OLD_DynamicMap|false
+//     */
+//    public function getMap($options = [])
+//    {
+//        // If the options are actually a string
+//        if (is_string($options)) {
+//            // Return existing map
+//            return ($this->_maps[$options] ?? false);
+//        }
+//
+//        // Generate a new dynamic map
+//        $map = new _OLD_DynamicMap($options);
+//
+//        // Alias for map ID
+//        $id =& $map->dna['id'];
+//
+//        // If no map ID, set one
+//        if (!$id) {
+//            $id = 'gm-map-'.++$this->_mapCounter;
+//        }
+//
+//        // Add new map to internal collection
+//        $this->_maps[$id] = $map;
+//
+//        // Return new map
+//        return $map;
+//    }
 
 }
