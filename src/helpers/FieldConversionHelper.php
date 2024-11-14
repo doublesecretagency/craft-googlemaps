2339b31974d172f81b1c8f13f8e4991471f49a93
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2024-11-13 16:23:29 -0800

    Improved Postgres compatibility

--- src/helpers/FieldConversionHelper.php
+++ src/helpers/FieldConversionHelper.php
@@ -44,6 +44,29 @@ class FieldConversionHelper
     ];
 
     /**
+     * Prefix the table name.
+     *
+     * @param string $table
+     * @return string
+     */
+    private static function _prefix(string $table): string
+    {
+        // Get the table prefix
+        $tablePrefix = Craft::$app->getConfig()->getDb()->tablePrefix;
+
+        // If no prefix, return the table name as-is
+        if (!$tablePrefix) {
+            return $table;
+        }
+
+        // Remove trailing underscore
+        $tablePrefix = rtrim($tablePrefix, '_');
+
+        // Return the prefixed table name
+        return "{$tablePrefix}_{$table}";
+    }
+
+    /**
      * Converts Address fields from the Mapbox (Double Secret Agency) plugin.
      *
      * @return void
@@ -86,20 +109,24 @@ class FieldConversionHelper
                     return;
                 }
 
+                // Define the table names
+                $gm_addresses = static::_prefix('googlemaps_addresses');
+                $mb_addresses = static::_prefix('mapbox_addresses');
+
                 // Merge and escape column names
                 $columns = '[['.implode(']],[[', self::$addressColumns).']]';
 
                 // Copy field's rows from `mapbox_addresses` into `googlemaps_addresses`
                 $sql = <<<SQL
-INSERT INTO [[googlemaps_addresses]] ({$columns})
+INSERT INTO [[{$gm_addresses}]] ({$columns})
 SELECT {$columns}
-FROM [[mapbox_addresses]]
+FROM [[{$mb_addresses}]]
 WHERE [[fieldId]] = :fieldId
   AND NOT EXISTS (
     SELECT 1
-    FROM [[googlemaps_addresses]]
-    WHERE [[googlemaps_addresses]].[[uid]] = [[mapbox_addresses]].[[uid]]
-)
+    FROM [[{$gm_addresses}]]
+    WHERE [[{$gm_addresses}]].[[uid]] = [[{$mb_addresses}]].[[uid]]
+);
 SQL;
 
                 // Execute the SQL statement
@@ -110,6 +137,8 @@ SQL;
             }
         );
     }
+
+    // ========================================================================= //
 
     /**
      * Converts Address fields from the Maps (Ether Creative) plugin.
@@ -158,73 +187,165 @@ SQL;
                 }
 
                 // Compile the column name
-                $column = ElementHelper::fieldColumn($field->columnPrefix, $field->handle, $field->columnSuffix);
+                $fieldColumn = ElementHelper::fieldColumn($field->columnPrefix, $field->handle, $field->columnSuffix);
 
-                // Merge and escape column names
-                $columns = '[['.implode(']],[[', self::$addressColumns).']]';
-
-                // Specify the content column
-                $contentColumn = "[[content]].[[{$column}]]";
-
-                // Copy field's data from the `content` table to `googlemaps_addresses`
-                $sql = <<<SQL
-INSERT INTO [[googlemaps_addresses]] ({$columns})
-
-SELECT
-    [[content]].[[elementId]] AS [[elementId]],
-    [[content]].[[siteId]] AS [[siteId]],
-    :fieldId AS [[fieldId]],
-    NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.address')), '') AS [[formatted]],
-    {$contentColumn} AS [[raw]],
-    NULL AS [[name]],
-    -- Combine street number with the first part of the address and replace empty string with NULL
-    NULLIF(
-        CONCAT(
-            JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.number')), ' ',
-            SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.address')), ', ', 1)
-        ), 
-        ''
-    ) AS [[street1]],
-    NULL AS [[street2]],
-    -- Replace empty strings with NULL for city, state, zip, county, and country
-    NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.city')), '') AS [[city]],
-    NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.state')), '') AS [[state]],
-    NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.postcode')), '') AS [[zip]],
-    NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.county')), '') AS [[county]],
-    NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.country')), '') AS [[country]],
-    -- Extract neighborhood (second part of the address, if exists)
-    CASE
-        WHEN JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.address')) LIKE '%,%'
-            THEN SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.parts.address')), ', ', -1)
-        ELSE NULL
-    END AS [[neighborhood]],
-    -- Extract lat and lng from JSON, handling 'null' strings
-    NULLIF(NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.lat')), ''), 'null') AS [[lat]],
-    NULLIF(NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.lng')), ''), 'null') AS [[lng]],
-    NULLIF(NULLIF(JSON_UNQUOTE(JSON_EXTRACT({$contentColumn}, '$.zoom')), ''), 'null') AS [[zoom]],
-    [[content]].[[dateCreated]],
-    [[content]].[[dateUpdated]],
-    [[content]].[[uid]]
-
-FROM [[content]]
-
-WHERE {$contentColumn} IS NOT NULL
-    AND NOT EXISTS (
-        SELECT 1
-        FROM [[googlemaps_addresses]]
-        WHERE [[googlemaps_addresses]].[[uid]] = [[content]].[[uid]]
-    )
-
-ORDER BY [[content]].[[elementId]] ASC, [[content]].[[siteId]] ASC
-SQL;
+                // If using MySQL
+                if (Craft::$app->getDb()->getIsMysql()) {
+                    $query = static::_queryMySqlMapsFields($fieldColumn);
+                } else {
+                    $query = static::_queryPostgresMapsFields($fieldColumn);
+                }
 
                 // Execute the SQL statement
-                Yii::$app->db->createCommand($sql)
+                Yii::$app->db->createCommand($query)
                     ->bindValues([':fieldId' => $field->id])
                     ->execute();
             }
         );
 
+    }
+
+    /**
+     * Returns the MySQL query for converting Maps fields to Google Maps fields.
+     *
+     * @param string $fieldColumn
+     * @return string
+     */
+    private static function _queryMySqlMapsFields(string $fieldColumn): string
+    {
+        // Define the table names
+        $content      = static::_prefix('content');
+        $gm_addresses = static::_prefix('googlemaps_addresses');
+
+        // Merge and escape column names
+        $addressColumns = '[['.implode(']],[[', self::$addressColumns).']]';
+
+        // Copy field's data from the `content` table to `googlemaps_addresses`
+        return <<<MYSQL
+INSERT INTO [[{$gm_addresses}]] ({$addressColumns})
+
+SELECT
+    [[{$content}]].[[elementId]] AS [[elementId]],
+    [[{$content}]].[[siteId]] AS [[siteId]],
+    :fieldId AS [[fieldId]],
+    NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.address')), '') AS [[formatted]],
+    [[{$content}]].[[{$fieldColumn}]] AS [[raw]],
+    NULL AS [[name]],
+    -- Combine street number with the first part of the address and replace empty string with NULL
+    NULLIF(
+        CONCAT(
+            JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.number')), ' ',
+            SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.address')), ', ', 1)
+        ), 
+        ''
+    ) AS [[street1]],
+    NULL AS [[street2]],
+    -- Replace empty strings with NULL for city, state, zip, county, and country
+    NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.city')), '') AS [[city]],
+    NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.state')), '') AS [[state]],
+    NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.postcode')), '') AS [[zip]],
+    NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.county')), '') AS [[county]],
+    NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.country')), '') AS [[country]],
+    -- Extract neighborhood (second part of the address, if exists)
+    CASE
+        WHEN JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.address')) LIKE '%,%'
+            THEN SUBSTRING_INDEX(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.parts.address')), ', ', -1)
+        ELSE NULL
+    END AS [[neighborhood]],
+    -- Extract lat and lng from JSON, handling 'null' strings
+    NULLIF(NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.lat')), ''), 'null') AS [[lat]],
+    NULLIF(NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.lng')), ''), 'null') AS [[lng]],
+    NULLIF(NULLIF(JSON_UNQUOTE(JSON_EXTRACT([[{$content}]].[[{$fieldColumn}]], '$.zoom')), ''), 'null') AS [[zoom]],
+    [[{$content}]].[[dateCreated]],
+    [[{$content}]].[[dateUpdated]],
+    [[{$content}]].[[uid]]
+
+FROM [[{$content}]]
+
+WHERE {$fieldColumn} IS NOT NULL
+    AND NOT EXISTS (
+        SELECT 1
+        FROM [[{$gm_addresses}]]
+        WHERE [[{$gm_addresses}]].[[uid]] = [[{$content}]].[[uid]]
+    )
+
+ORDER BY [[{$content}]].[[elementId]] ASC, [[{$content}]].[[siteId]] ASC;
+MYSQL;
+    }
+
+    /**
+     * Returns the Postgres query for converting Maps fields to Google Maps fields.
+     *
+     * @param string $fieldColumn
+     * @return string
+     */
+    private static function _queryPostgresMapsFields(string $fieldColumn): string
+    {
+        // Define the table names
+        $content      = static::_prefix('content');
+        $gm_addresses = static::_prefix('googlemaps_addresses');
+
+        // Merge and escape column names
+        $addressColumns = '[['.implode(']],[[', self::$addressColumns).']]';
+
+        // Copy field's data from the `content` table to `googlemaps_addresses`
+        return <<<POSTGRES
+INSERT INTO [[{$gm_addresses}]] ({$addressColumns})
+
+SELECT
+    [[{$content}]].[[elementId]] AS [[elementId]],
+    [[{$content}]].[[siteId]] AS [[siteId]],
+    :fieldId AS [[fieldId]],
+    NULLIF(([[{$content}]].[[{$fieldColumn}]]::jsonb)->>'address', '') AS [[formatted]],
+    [[{$content}]].[[{$fieldColumn}]] AS [[raw]],
+    NULL AS [[name]],
+    -- Use COALESCE to handle NULLs in street1 concatenation
+    NULLIF(
+        COALESCE(([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'number', '') || ' ' || 
+        COALESCE(SPLIT_PART(([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'address', ', ', 1), ''), 
+        ''
+    ) AS [[street1]],
+    NULL AS [[street2]],
+    -- Replace empty strings with NULL for city, state, zip, county, and country
+    NULLIF(([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'city', '') AS [[city]],
+    NULLIF(([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'state', '') AS [[state]],
+    NULLIF(([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'postcode', '') AS [[zip]],
+    NULLIF(([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'county', '') AS [[county]],
+    NULLIF(([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'country', '') AS [[country]],
+    -- Extract neighborhood (second part of the address, if exists)
+    CASE
+        WHEN ([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'address' LIKE '%,%'
+            THEN SPLIT_PART(([[{$content}]].[[{$fieldColumn}]]::jsonb)->'parts'->>'address', ', ', 2)
+        ELSE NULL
+    END AS [[neighborhood]],
+    -- Extract lat and lng from JSON, with regex check for numeric values
+    CASE 
+        WHEN ([[{$content}]].[[{$fieldColumn}]]::jsonb)->>'lat' ~ '^-?\d+(\.\d+)?$' 
+        THEN (([[{$content}]].[[{$fieldColumn}]]::jsonb)->>'lat')::numeric 
+        ELSE NULL 
+    END AS [[lat]],
+    CASE 
+        WHEN ([[{$content}]].[[{$fieldColumn}]]::jsonb)->>'lng' ~ '^-?\d+(\.\d+)?$' 
+        THEN (([[{$content}]].[[{$fieldColumn}]]::jsonb)->>'lng')::numeric 
+        ELSE NULL 
+    END AS [[lng]],
+    CASE 
+        WHEN ([[{$content}]].[[{$fieldColumn}]]::jsonb)->>'zoom' ~ '^\d+(\.\d+)?$' 
+        THEN (([[{$content}]].[[{$fieldColumn}]]::jsonb)->>'zoom')::numeric 
+        ELSE NULL 
+    END AS [[zoom]],
+    [[{$content}]].[[dateCreated]],
+    [[{$content}]].[[dateUpdated]],
+    [[{$content}]].[[uid]]
+FROM [[{$content}]]
+WHERE [[{$content}]].[[{$fieldColumn}]] IS NOT NULL
+    AND NOT EXISTS (
+        SELECT 1
+        FROM [[{$gm_addresses}]]
+        WHERE [[{$gm_addresses}]].[[uid]] = [[{$content}]].[[uid]]
+    )
+ORDER BY [[{$content}]].[[elementId]] ASC, [[{$content}]].[[siteId]] ASC;
+POSTGRES;
     }
 
 }
