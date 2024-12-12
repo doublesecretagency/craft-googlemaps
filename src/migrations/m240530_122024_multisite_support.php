4752408aec306b29fe2447f731c47860c064f7a3
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2024-12-11 20:13:36 -0800

    Patched bug in multisite migration

--- src/migrations/m240530_122024_multisite_support.php
+++ src/migrations/m240530_122024_multisite_support.php
@@ -39,14 +39,16 @@ class m240530_122024_multisite_support extends Migration
         // Add new columns
         $this->_newColumns();
 
-        // Add new column indexes
-        $this->_newIndexes();
-
         // Populate new columns with existing data
         $this->_populateData();
 
-        // Make new column not nullable, AFTER the column has been populated
+        // Make new column not nullable
+        //  - AFTER the column has been populated
+        //  - BEFORE adding indexes and foreign keys
         $this->alterColumn(Install::GM_ADDRESSES, 'siteId', $this->integer()->notNull());
+
+        // Add new column indexes
+        $this->_newIndexes();
 
         // Add new foreign keys
         $this->_newForeignKeys();
